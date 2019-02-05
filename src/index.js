d16469a8ef3d82ed49f5dfbd02e6ee1419acbe19
Anton <anton@adc.sh>
2019-02-05 14:34:24 +0300

    add src and test

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import { debuglog } from 'util'
-
-const LOG = debuglog('resolve-dependency')
+import exists from '@wrote/exists'
+import { dirname, join } from 'path'
 
 /**
- * Find The File That Dependency Path Resolves To.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * For the given local path that can omit the JS/JSX extension and point to a directory (e.g., `./lib` or `./lib/example`), find that file on the filesystem.
+ * @param {string} path The dependency path, to resolve e.g., `./lib`.
+ * @param {string} [relativeFrom] The optional path of the file that imports the given path.
  */
-export default async function resolveDependency(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('resolve-dependency called with %s', text)
-  return text
+const resolveDependency = async (path, relativeFrom) => {
+  if (relativeFrom) {
+    const d = dirname(relativeFrom)
+    path = join(d, path)
+  }
+  let e = await exists(path)
+  let res = path
+  let isDir = false
+  if (!e) {
+    let p = `${path}.js`
+    e = await exists(p)
+    if (!e) p = `${path}.jsx`; e = await exists(p)
+    if (!e) throw new Error(`${path}.js or ${path}.jsx is not found.`)
+    res = p
+  } else if (e.isDirectory()) {
+    let p = `${path}/index.js`
+    e = await exists(p)
+    if (!e) p = `${p}x`; e = await exists(p)
+    if (!e) throw new Error(`index.jsx? file is not found in ${path}.`)
+    res = p
+    isDir = true
+  }
+  return { path: res, isDir }
 }
 
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
+export default resolveDependency
--- test/spec/default.js
+++ test/spec/default.js
@@ -8,14 +8,25 @@ const T = {
   'is a function'() {
     equal(typeof resolveDependency, 'function')
   },
-  async 'calls package without error'() {
-    await resolveDependency()
+  async 'resolves js index'() {
+    const { isDir, path } = await resolveDependency('test/fixture/js')
+    ok(isDir)
+    equal(path, 'test/fixture/js/index.js')
   },
-  async 'gets a link to the fixture'({ FIXTURE }) {
-    const res = await resolveDependency({
-      text: FIXTURE,
-    })
-    ok(res, FIXTURE)
+  async 'resolves jsx index'() {
+    const { isDir, path } = await resolveDependency('test/fixture/jsx')
+    ok(isDir)
+    equal(path, 'test/fixture/jsx/index.jsx')
+  },
+  async 'resolves existing file'() {
+    const { isDir, path } = await resolveDependency('test/fixture/package.json')
+    ok(!isDir)
+    equal(path, 'test/fixture/package.json')
+  },
+  async 'resolves relative dep'() {
+    const { isDir, path } = await resolveDependency('./jsx', 'test/fixture/index.js')
+    ok(isDir)
+    equal(path, 'test/fixture/jsx/index.jsx')
   },
 }
 
