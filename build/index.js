8a596fdefe575ae124b9141ea4cb7c2fafcef97d
Anton <anton@adc.sh>
2019-02-05 14:54:02 +0300

    b

--- build/index.js
+++ build/index.js
@@ -1,30 +1,37 @@
-const { debuglog } = require('util');
-
-const LOG = debuglog('resolve-dependency')
+let exists = require('@wrote/exists'); if (exists && exists.__esModule) exists = exists.default;
+const { dirname, join, relative } = require('path');
 
 /**
- * Find The File That Dependency Path Resolves To.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
+ * For the given local path that can omit the JS/JSX extension and point to a directory (e.g., `./lib` or `./lib/example`), find that file on the filesystem.
+ * @param {string} path The dependency path, to resolve e.g., `./lib`.
+ * @param {string} [relativeFrom] The optional path of the file that imports the given path.
  */
-               async function resolveDependency(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('resolve-dependency called with %s', text)
-  return text
+const resolveDependency = async (path, relativeFrom) => {
+  if (relativeFrom) {
+    const d = dirname(relativeFrom)
+    path = join(d, path)
+  }
+  let e = await exists(path)
+  let res = path
+  let isDir = false
+  if (!e) {
+    let p = `${path}.js`
+    e = await exists(p)
+    if (!e) p = `${path}.jsx`; e = await exists(p)
+    if (!e) throw new Error(`${path}.js or ${path}.jsx is not found.`)
+    res = p
+  } else if (e.isDirectory()) {
+    let p = `${path}/index.js`
+    e = await exists(p)
+    if (!e) p = `${p}x`; e = await exists(p)
+    if (!e) throw new Error(`index.jsx? file is not found in ${path}.`)
+    res = p
+    isDir = true
+  }
+  return {
+    path: path.startsWith('.') ? relative('', res) : res,
+    isDir,
+  }
 }
 
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
-
-
-module.exports = resolveDependency
-//# sourceMappingURL=index.js.map
+module.exports=resolveDependency
